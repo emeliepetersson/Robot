3026547db5b3fbbe6c2273abf81fecbe651589d9
emeliepetersson <54805510+emeliepetersson@users.noreply.github.com>
2024-03-09 10:40:26 +0100

    move modal logic to its own files

---
+++ src/helpers/modal/modal.ts
@@ -0,0 +1,45 @@
+import { ModalButtonCb } from "./modal.types";
+
+/**
+ * Shows a modal with a title, message and button
+ * 
+ * @param {string} title
+ * @param {string} message
+ * @param {string} buttonText
+ * @param {ModalButtonCb} buttonCallback
+ * @returns {void}
+ */
+const showModal = (title: string, message: string, buttonText: string, buttonCallback: ModalButtonCb): void => {
+    const backdrop = document.getElementById('backdrop');
+    const modal = document.getElementById('modal');
+    const titleElement = modal!.querySelector('h2');
+    const messageElement = modal!.querySelector('p');
+    const button = modal!.querySelector('button');
+
+    titleElement!.textContent = title;
+    messageElement!.textContent = message;
+    button!.textContent = buttonText;
+    button!.onclick = () => {
+        buttonCallback();
+        closeModal();
+    };
+
+    
+    backdrop!.style.display = 'block';
+    modal!.style.display = 'block';
+}
+
+/**
+ * Close the modal
+ * 
+ * @returns {void}
+ */
+const closeModal = (): void => {
+    const backdrop = document.getElementById('backdrop');
+    const modal = document.getElementById('modal');
+
+    backdrop!.style.display = 'none';
+    modal!.style.display = 'none';
+}
+
+export { showModal };
