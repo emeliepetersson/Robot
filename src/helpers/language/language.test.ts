8c9620aa853ff564b4e079c0bafbd2c10527c13b
emeliepetersson <54805510+emeliepetersson@users.noreply.github.com>
2024-03-14 17:16:08 +0100

    add test for language of the command button text

--- src/helpers/language/language.test.ts
+++ src/helpers/language/language.test.ts
@@ -1,12 +1,30 @@
 import { mockDocumentBody } from '../../../mocks/domMocks';
+import { setupDialogue } from '../dialogue/dialogue';
 import { setupLanguageButton, texts } from './language';
 import { en, sv } from './language.types';
 
-mockDocumentBody();
-
 describe('Language', () => {
+    beforeEach(()=> {
+        // Reset the document body
+        mockDocumentBody();
+    })
+
     it('should return an object with swedish texts by default', () => {
         expect(texts).toEqual(sv);
+    });
+
+    it('should update the command button text correctly if it is clicked', () => {
+        const languageButton = document.querySelector<HTMLButtonElement>('#language')!
+        setupLanguageButton(languageButton);
+
+        const commandButton = document.querySelector<HTMLButtonElement>('#dialogue')!;
+        setupDialogue(commandButton);
+
+        commandButton.click();
+        expect(commandButton.textContent).toBe(sv.commandButtonDone);
+
+        languageButton.click();
+        expect(commandButton.textContent).toBe(en.commandButtonDone);
     });
 
     it('should change the language when the button is clicked', () => {
@@ -19,6 +37,9 @@ describe('Language', () => {
 
         // ...and the texts should be updated to english
         const commandsTitle = document.querySelector<HTMLHeadingElement>('.card.commands h2')!;
+        const positionCard = document.querySelector<HTMLDivElement>('.card.position')!;
+        const positionTitle = positionCard.querySelector<HTMLHeadingElement>('h2')!;
         expect(commandsTitle.textContent).toBe(en.commandsTitle);
+        expect(positionTitle.textContent).toBe(en.positionTitle);
     });
 });
