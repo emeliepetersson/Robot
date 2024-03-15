8c952b50b2700d8280d8eacc7a26278662e14d0f
emeliepetersson <54805510+emeliepetersson@users.noreply.github.com>
2024-03-15 15:58:32 +0100

    fix repetitive code

--- src/helpers/modal/modal.test.ts
+++ src/helpers/modal/modal.test.ts
@@ -1,54 +1,46 @@
 import { mockDocumentBody } from "../../../mocks/domMocks";
 import { showModal } from "./modal";
 
-mockDocumentBody();
-
 // Mock callback for the modal button
 const modalButtonCb = jest.fn();
 
+let backdrop: HTMLElement;
+let modal: HTMLElement;
+let modalButton: HTMLButtonElement;
+
+
 describe('Modal', () => {
-    it('should display a modal', () => {
+    beforeAll(() => {
+        mockDocumentBody();
         showModal('Title', 'Message', 'Button', modalButtonCb);
 
-        const backdrop = document.getElementById('backdrop')!;
-        const modal = document.getElementById('modal')!;
+        backdrop = document.getElementById('backdrop')!;
+        modal = document.getElementById('modal')!;
+        modalButton = modal.querySelector('button')!;
+    })
 
+    it('should display a modal', () => {
         expect(backdrop.style.display).toBe('block');
         expect(modal.style.display).toBe('block');
     });
 
     it('should call given callback when clicking on button in modal', () => {
-        showModal('Title', 'Message', 'Button', modalButtonCb);
-
-        const modal = document.getElementById('modal')!;
-        const button = modal.querySelector('button')!;
-
-        button.click();
+        modalButton.click();
 
         expect(modalButtonCb).toHaveBeenCalledTimes(1);
     });
 
     it('should display given title, message and button text in modal', () => {
-        showModal('Title', 'Message', 'Button', modalButtonCb);
-
-        const modal = document.getElementById('modal')!;
         const title = modal.querySelector('h2')!;
         const message = modal.querySelector('p')!;
-        const button = modal.querySelector('button')!;
 
         expect(title.textContent).toBe('Title');
         expect(message.textContent).toBe('Message');
-        expect(button.textContent).toBe('Button');
+        expect(modalButton.textContent).toBe('Button');
     });
 
     it('should close the modal when clicking on the button', () => {
-        showModal('Title', 'Message', 'Button', modalButtonCb);
-
-        const modal = document.getElementById('modal')!;
-        const backdrop = document.getElementById('backdrop')!;
-        const button = modal.querySelector('button')!;
-
-        button.click();
+        modalButton.click();
 
         expect(modal.style.display).toBe('none');
         expect(backdrop.style.display).toBe('none');
